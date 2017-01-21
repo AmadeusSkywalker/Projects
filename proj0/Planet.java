ef7367d7e99e2b09474b0a6490dedb00f1078914
vip <vip@airbears2-10-142-176-78.airbears2.1918.berkeley.edu>
2017-01-21 11:14:26 -0800

    Completed proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,17 @@
 public class Planet {
+  double xxPos;
+  double yyPos;
+  double xxVel;
+  double yyVel;
+  double mass;
+  String imgFileName;
+  public Planet(double xP, double yP, double xV,double yV, double m, String img){
+    xxPos=xP;
+    yyPos=yp;
+    xxVel=xV;
+    yyVel=yV;
+    mass=m;
+    imgFileName=img;
+  }
+  
 }
