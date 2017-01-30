a78894e436f792d13754957fec1cb2a4ff0f642d
vip <vip@airbears2-10-142-176-36.airbears2.1918.berkeley.edu>
2017-01-29 21:31:43 -0800

    May the force be with you

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,11 +1,11 @@
 public class LinkedListDeque<Type>{
 
    private class StuffNode{
-     public Type stuff;
+     public Type item;
      public StuffNode next;
      public StuffNode prev;
-     public StuffNode(item,a,b){
-       stuff=item;
+     public StuffNode(Type c,StuffNode a,StuffNode b){
+       item=c;
        next=a;
        prev=b;
      }
@@ -27,7 +27,94 @@ public class LinkedListDeque<Type>{
 
    public void addFirst(Type x){
      if (size==0){
-       
+       Sentinel.next=new StuffNode(x,Sentinel,Sentinel);
+       Sentinel.prev=Sentinel.next;
+       size=size+1;
+     }
+     else {
+         Sentinel.next=new StuffNode(x,Sentinel.next,Sentinel);
+         Sentinel.next.next.prev=Sentinel.next;
+         size+=1;
      }
    }
+
+   public void addLast(Type x){
+       if (size==0){
+           Sentinel.next=new StuffNode(x,Sentinel,Sentinel);
+           Sentinel.prev=Sentinel.next;
+           size=size+1;
+       }
+       else{
+           Sentinel.prev=new StuffNode(x,Sentinel,Sentinel.prev);
+           Sentinel.prev.prev.next=Sentinel.prev;
+           size+=1;
+       }
+   }
+
+   public boolean isEmpty(){
+       return size==0;
+   }
+
+   public int size(){
+       return size;
+   }
+
+   public void printDeque(){
+       int index=1;
+       StuffNode pointer=Sentinel.next;
+       while (index<=size){
+           System.out.print(pointer.item+" ");
+           index=index+1;
+           pointer=pointer.next;
+       }
+       System.out.println();
+   }
+
+   public Type removeFirst(){
+       if (size==0){
+           return null;
+       }
+       StuffNode pointer=Sentinel.next;
+       Sentinel.next=Sentinel.next.next;
+       size=size-1;
+       return pointer.item;
+   }
+
+   public Type removeLast(){
+       if (size==0){
+           return null;
+       }
+       StuffNode pointer=Sentinel.prev;
+       Sentinel.prev=Sentinel.prev.prev;
+       size=size-1;
+       return pointer.item;
+   }
+
+   public Type get(int x){
+       StuffNode pointer=Sentinel.next;
+       if (x>=size){
+           return null;
+       }
+       int index=x;
+       while(index>0){
+           pointer=pointer.next;
+           index=index-1;
+       }
+       return pointer.item;
+   }
+
+   public Type getRecursive(int x){
+       if (x>=size){
+           return null;
+       }
+       return recursehelp(x,Sentinel.next);
+   }
+
+   public Type recursehelp(int x, StuffNode m){
+       if (x==0){
+           return m.item;
+       }
+       return recursehelp(x-1,m.next);
+   }
+
 }
--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -35,8 +35,7 @@ public class LinkedListDequeTest {
 	  * && is the "and" operation. */
 	public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
+
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -58,7 +57,7 @@ public class LinkedListDequeTest {
 		lld1.printDeque();
 
 		printTestStatus(passed);
-		*/
+
 	}
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
@@ -66,8 +65,6 @@ public class LinkedListDequeTest {
 
 		System.out.println("Running add/remove test.");
 
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -80,13 +77,34 @@ public class LinkedListDequeTest {
 		// should be empty 
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
+		lld1.addLast(9);
+		passed=checkSize(1,lld1.size())&&passed;
+
+		lld1.removeLast();
+		// should be empty
+		passed = checkEmpty(true, lld1.isEmpty()) && passed;
+
 		printTestStatus(passed);
-		*/
+
+	}
+
+	public static void getTest(){
+		boolean passed=true;
+		System.out.println("Running get test.\n");
+		LinkedListDeque<String> jedi = new LinkedListDeque<String>();
+		String power="force";
+		String weapon="lightsaber";
+		jedi.addFirst(power);
+		jedi.addLast(weapon);
+		passed=(jedi.get(0)==power)&&passed;
+		passed=(jedi.get(1)==weapon)&&passed;
+		printTestStatus(passed);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Running tests.\n");
 		addIsEmptySizeTest();
 		addRemoveTest();
+		getTest();
 	}
 } 
