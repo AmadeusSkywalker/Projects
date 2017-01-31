b85736b19469d6622d698bbe39debd1c2150358c
vip <vip@calvisitor-10-105-139-98.calvisitor.1918.berkeley.edu>
2017-01-31 12:34:07 -0800

    May the force be with you

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -12,35 +12,29 @@ public class ArrayDeque<Type> {
     }
 
     public void resize(int capacity) {
-        /*
         Type[] a=(Type[]) new Object[capacity];
         System.arraycopy(items,nextFirst+1,a,capacity-items.length+nextFirst+1,items.length-nextFirst-1);
         System.arraycopy(items,0,a,0,nextLast);
         nextFirst=capacity-items.length+nextFirst;
         items=a;
-        */
     }
 
     public void addFirst(Type x) {
-        /*
         if(nextFirst==nextLast){
             resize(size*2);
         }
         items[nextFirst]=x;
         nextFirst=(nextFirst-1+items.length)%items.length;
         size=size+1;
-        */
     }
 
     public void addLast(Type x) {
-        /*
         if(nextFirst==nextLast){
             resize(size*2);
         }
         items[nextLast]=x;
         nextLast=(nextLast+1+items.length)%items.length;
         size=size+1;
-        */
     }
 
     public boolean isEmpty() {
@@ -52,17 +46,14 @@ public class ArrayDeque<Type> {
     }
 
     public void printDeque() {
-        /*
         for (int i=0;i<items.length;i++){
             System.out.print(items[i]+" ");
         }
         System.out.println();
         System.out.println();
-        */
     }
 
     public Type removeFirst() {
-        /*
         int index=(nextFirst+1+items.length)%items.length;
         Type result=items[index];
         if (result==null){
@@ -72,12 +63,9 @@ public class ArrayDeque<Type> {
         nextFirst=index;
         size=size-1;
         return result;
-        */
-        return null;
     }
 
     public Type removeLast() {
-        /*
         int index=(nextLast-1+items.length)%items.length;
         Type result=items[index];
         if(result==null){
@@ -87,18 +75,13 @@ public class ArrayDeque<Type> {
         nextLast=index;
         size=size-1;
         return result;
-        */
-        return null;
     }
 
     public Type get(int index) {
-        /*
         if (index>=size){
             return null;
         }
         return items[(nextFirst+index+1)%items.length];
-        */
-        return null;
     }
 
     }
