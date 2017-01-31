16c46f97b4a3405c6dd192a2fb45919c61d65b0c
vip <vip@calvisitor-10-105-139-98.calvisitor.1918.berkeley.edu>
2017-01-31 12:30:49 -0800

    May the force be with you

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,59 +1,68 @@
-public class ArrayDeque<Type>{
+public class ArrayDeque<Type> {
     private Type[] items;
     private int size;
     private int nextFirst;
     private int nextLast;
 
-    public ArrayDeque(){
-        items=(Type[]) new Object[8];
-        size=0;
-        nextFirst=items.length/2;
-        nextLast=nextFirst+1;
+    public ArrayDeque() {
+        items = (Type[]) new Object[8];
+        size = 0;
+        nextFirst = items.length / 2;
+        nextLast = nextFirst + 1;
     }
 
-    public void resize(int capacity){
+    public void resize(int capacity) {
+        /*
         Type[] a=(Type[]) new Object[capacity];
         System.arraycopy(items,nextFirst+1,a,capacity-items.length+nextFirst+1,items.length-nextFirst-1);
         System.arraycopy(items,0,a,0,nextLast);
         nextFirst=capacity-items.length+nextFirst;
         items=a;
+        */
     }
 
-    public void addFirst(Type x){
+    public void addFirst(Type x) {
+        /*
         if(nextFirst==nextLast){
             resize(size*2);
         }
         items[nextFirst]=x;
         nextFirst=(nextFirst-1+items.length)%items.length;
         size=size+1;
+        */
     }
 
-    public void addLast(Type x){
+    public void addLast(Type x) {
+        /*
         if(nextFirst==nextLast){
             resize(size*2);
         }
         items[nextLast]=x;
         nextLast=(nextLast+1+items.length)%items.length;
         size=size+1;
+        */
     }
 
-    public boolean isEmpty(){
-        return size==0;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
+    public void printDeque() {
+        /*
         for (int i=0;i<items.length;i++){
             System.out.print(items[i]+" ");
         }
         System.out.println();
         System.out.println();
+        */
     }
 
-    public Type removeFirst(){
+    public Type removeFirst() {
+        /*
         int index=(nextFirst+1+items.length)%items.length;
         Type result=items[index];
         if (result==null){
@@ -63,9 +72,12 @@ public class ArrayDeque<Type>{
         nextFirst=index;
         size=size-1;
         return result;
+        */
+        return null;
     }
 
-    public Type removeLast(){
+    public Type removeLast() {
+        /*
         int index=(nextLast-1+items.length)%items.length;
         Type result=items[index];
         if(result==null){
@@ -75,13 +87,18 @@ public class ArrayDeque<Type>{
         nextLast=index;
         size=size-1;
         return result;
+        */
+        return null;
     }
 
-    public Type get(int index){
+    public Type get(int index) {
+        /*
         if (index>=size){
             return null;
         }
         return items[(nextFirst+index+1)%items.length];
+        */
+        return null;
     }
 
-}
+    }
