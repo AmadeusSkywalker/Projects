d6ee0d8f337cd716c7e7ba8421a3ac9ef30583fa
vip <vip@airbears2-10-142-178-92.airbears2.1918.berkeley.edu>
2017-04-27 22:13:00 -0700

    may the force be with you

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 public class HuffmanDecoder {
     public static void main(String[] args) {
         ObjectReader or = new ObjectReader(args[0]);
-        ObjectWriter ow = new ObjectWriter(args[1]);
         BinaryTrie trie = (BinaryTrie) or.readObject();
         BitSequence hugebits = (BitSequence) or.readObject();
         ArrayList<Match> matchlist = new ArrayList<>();
@@ -17,6 +16,10 @@ public class HuffmanDecoder {
             int index = getout.length();
             hugebits = hugebits.allButFirstNBits(index);
         }
-        ow.writeObject(matchlist);
+        char[] result = new char[matchlist.size()];
+        for (int i = 0; i < matchlist.size(); i++) {
+            result[i] = matchlist.get(i).getSymbol();
+        }
+        FileUtils.writeCharArray(args[1], result);
     }
 }
